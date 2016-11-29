d7d6956ba4fad7234fc585d762996737ad1dad5d
Oorja Chowdhary <oorjac@uw.edu>
2016-11-29 13:19:25 -0800

    added firebase code

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+var config = {
+    apiKey: "AIzaSyBy8agFrVxo1uJkJFuqhLUYIaEpwCPry1A",
+    authDomain: "firebaes-project.firebaseapp.com",
+    databaseURL: "https://firebaes-project.firebaseio.com",
+    storageBucket: "firebaes-project.appspot.com",
+    messagingSenderId: "674443723360"
+  };
+  firebase.initializeApp(config);
+  
 ReactDOM.render(
   <App />,
   document.getElementById('root')
