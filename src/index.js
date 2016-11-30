bd8c01412fa67c482034ba86bea34a2746f508c9
Hannah Mansoori <Banana@D-108-179-150-222.dhcp4.washington.edu>
2016-11-30 10:58:33 -0800

    Add general routing structure and add route for sign-up

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './Components/App';
+import SignUpForm from './Components/SignUp';
 
 import firebase from 'firebase';
-import 
+import {Route, Router, hashHistory, IndexRoute} from 'react-router';
 import './index.css';
 
 var config = {
@@ -15,7 +16,11 @@ var config = {
   };
   firebase.initializeApp(config);
   
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+ReactDOM.render((
+  <Router history={hashHistory}>
+    <Route path='/' component={App}/>
+      <Route path='join' component={SignUpForm}>
+    </Route>
+  </Router>
+),document.getElementById('root')
 );
