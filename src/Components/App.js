488d07ac2e4176b8c1aad4882b8cc27cccd3fbf2
Hannah Mansoori <Banana@D-173-250-152-46.dhcp4.washington.edu>
2016-11-30 15:01:04 -0800

    Add button that takes users to login route, move search bar

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
 import '../css/App.css';
-import {Col, Image, Form, FormControl, InputGroup, Button, Glyphicon, ListGroup, ListGroupItem} from 'react-bootstrap';
+import {Col, Image, Form, FormControl, InputGroup, Button, Glyphicon} from 'react-bootstrap';
 import firebase from 'firebase';
-import {hashHistory} from 'react-router';
+import {hashHistory, Link} from 'react-router';
 
 class App extends Component {
   constructor(props) {
@@ -21,7 +21,7 @@ class App extends Component {
       else {
         console.log('Auth state changed: logged out');
         this.setState({ userId: null, show: false }); //null out the saved state
-        hashHistory.push('/login');
+        //hashHistory.push('/login');
       }
     })
   }
@@ -37,46 +37,23 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <h1>News Reviews</h1>
+          {!this.state.userId &&
+            <div className="login">
+              <button className="btn btn-primary"><Link to='login'>Login</Link></button>
+            </div>  
+          }
           {this.state.userId &&  /*inline conditional rendering*/
             <div className="logout">
               <button className="btn btn-warning" onClick={() => this.signOut() }>Sign out {firebase.auth().currentUser.displayName}</button>
             </div>
           }
          </div>
-        <SearchForm />
         {this.props.children}
       </div>
     );
   }
 }
 
-class SearchForm extends React.Component {
-    handleChange(event) {
-    var newValue = event.target.value;
-    newValue = newValue.toLowerCase();
-    var searchTerm = newValue;
-    this.setState({ searchValue: searchTerm });
-    console.log(newValue);
-  }
 
-  handleClick(event) {
-    console.log('click!');
-  }
-  
-  render() {
-    return (
-      <Form inline className="search">
-        <InputGroup>
-          <InputGroup.Button>
-            <Button onClick={this.props.searchClick}>
-              <Glyphicon glyph="search" aria-label="Search"/>
-            </Button>
-          </InputGroup.Button>
-          <FormControl type="text" placeholder="Search..." onChange = {this.props.handleChange}/>
-        </InputGroup>
-      </Form>
-    );
-  }
-}
 
 export default App;
