2d38a15772cd00e3fc0bf41b84c10128f76273d6
Allen Cho <allencho@uw.edu>
2016-12-03 13:15:36 -0800

    basic rating part done

--- src/Components/Rating.js
+++ src/Components/Rating.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import StarRating from 'react-star-rating'
-import {ButtonToolbar, Button, Modal } from 'react-bootstrap';
+import StarRatingComponent from 'react-star-rating-component';
+import {Button, Modal } from 'react-bootstrap';
 //$ npm install react-star-rating --save
 //<link rel="stylesheet" href="node_modules/react-star-rating/dist/css/react-star-rating.min.css"> in css file
 
@@ -8,11 +8,13 @@ class Rating extends React.Component {
     constructor() {
         super();
         this.state = {
-            show: false
-        }
+            show: false,
+            rating: 1
+        };
 
         this.showModal = this.showModal.bind(this);
-        this.showModal = this.hideModal.bind(this);
+        this.hideModal = this.hideModal.bind(this);
+        this.onStarClick = this.onStarClick.bind(this);
     }
 
     showModal() {
@@ -21,31 +23,52 @@ class Rating extends React.Component {
     hideModal() {
         this.setState({show: false});
     }
+    onStarClick(nextValue, prevValue, name) {
+        this.setState({rating: nextValue});
+    }
 
     render() {
+        const { rating } = this.state;
         return (
-            <ButtonToolbar>
+            <div>
                 <Button bsStyle="primary" onClick={this.showModal}>
                     Rate it
                 </Button>
-                <Modal {...this.props} show={this.state.show} onHide={this.hideModal} dialogClassName="custom-modal">
+                <Modal show={this.state.show} onHide={this.hideModal} dialogClassName="custom-modal">
                     <Modal.Header closeButton>
                         <Modal.Title id="contained-modal-title">Rate</Modal.Title>
                     </Modal.Header>
                     <Modal.Body>
                         <form target="_self" method="GET">
-                            <StarRating id="author" name="react-star-rating" caption="Author" totalStars={5} />
-                            <StarRating id="source" name="react-star-rating" caption="Source" totalStars={5} />
-                            <StarRating id="content" name="react-star-rating" caption="Content" totalStars={5} />
+                            <div>
+                                <p>Author: </p>
+                                <StarRatingComponent 
+                                    name="rate1" 
+                                    starCount={5}
+                                    value={rating}
+                                />
+                                <p>Source: </p>
+                                <StarRatingComponent 
+                                    name="rate2" 
+                                    starCount={5}
+                                    value={rating}
+                                />
+                                <p>Content: </p>
+                                <StarRatingComponent 
+                                    name="rate3" 
+                                    starCount={5}
+                                    value={rating}
+                                />
+                            </div>
                             <label>Your Review<input type="text"/></label>
                         </form>
                     </Modal.Body>
                     <Modal.Footer>
-                        <button type="submit" className="btn btn-primary">Submit Rating</button>
                         <Button onClick={this.hideModal}>Close</Button>
+                        <button type="submit" className="btn btn-primary">Submit Rating</button>
                     </Modal.Footer>
                 </Modal>
-            </ButtonToolbar>
+            </div>
         );
     }
 }
